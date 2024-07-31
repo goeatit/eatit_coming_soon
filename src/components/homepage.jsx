abe02162c0fc6fac1a24920d385fed601829772e
goeatit <94214883+goeatit@users.noreply.github.com>
2024-07-31 22:14:33 +0530

    Update homepage.jsx

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -9,7 +9,7 @@ import "./style.css";
 import "boxicons";
 
 function DemoCarousel() {
-  const deadline = new Date("2024-01-03");
+  const deadline = new Date("2024-10-01");
   deadline.setDate(deadline.getDate() + 60);
 
   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
